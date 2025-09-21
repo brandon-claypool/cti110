297a8b13f0b0af698e5f8b00928b612707916fb0
Brandon Claypool <claypoob3195@student.faytechcc.edu>
2025-09-21 13:25:47 -0500

    Upload P1LAB1

---
+++ P1LAB1_Claypool_Brandon.py
@@ -0,0 +1,9 @@
+ # Brandon Claypool
+ # 09/21/25
+ # P1LAB1
+ # This program will welcome the new user to the course.
+
+user_first_name = input("Please enter your first name: ")
+user_last_name = input("Please enter your last name: ")
+user_full_name = user_first_name + " " + user_last_name
+print('Hello,', user_full_name+',', 'and welcome to CTI-110!')
