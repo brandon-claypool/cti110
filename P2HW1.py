a975082270f1f58f416bbf9e9135b92a08275f39
Brandon Claypool <claypoob3195@student.faytechcc.edu>
2025-10-12 21:36:27 +0000

    finish p2hw1

--- P2HW1.py
+++ P2HW1.py
@@ -1,33 +1,33 @@
 # Brandon Claypool
 # 10/12/2025
-# P2HW2
-# Python program helps a user plan for a trip
+# P2HW1
+# Edit & enhance the P1HW2 travel-budget program to display nicely formatted results.
 
-# Pseudocode
-# First, tell the user the purposes of the program
-# Next, have the user input all the necessary information
-# Display a summary of the travel expenses
-# Output the remaining balance after expenses
-
-# First, tell the user the purpose of the program
 print("This program calculates and displays travel expenses")
 
-# Next, have the user input all the necessary information
-budget = int(input("\nEnter Budget: "))
-destination = input("\nEnter your travel destination: ")
-gas_money = int(input("\nHow much do you think you will spend on gas? "))
-hotel_money = int(input("\nApproximately, how much will you need for accommodation/hotel? "))
-food_money = int(input("\nLast, how much do you need for food? "))
+# Inputs as floats
+budget       = float(input("\nEnter Budget: "))
+destination  = input("\nEnter your travel destination: ")
+gas_money    = float(input("\nHow much do you think you will spend on gas? "))
+hotel_money  = float(input("\nApproximately, how much will you need for accommodation/hotel? "))
+food_money   = float(input("\nLast, how much do you need for food? "))
 
-# Display a summary of the travel expenses
-print("\n------------Travel Expenses------------")
-print("Location:", destination)
-print("Initial Budget:",budget)
-print("\nFuel:",gas_money)
-print("Accommodation:",hotel_money)
-print("Food:",food_money)
-
-# Output the remaining balance after expenses
 remaining_balance = budget - gas_money - hotel_money - food_money
 
-print("\nRemaining Balance:",remaining_balance)
+def money(x) -> str:  # defines a helper that formats a number as a dollar string
+    return f"${x:.2f}" # returns a new string with '$' and 2 decimals
+
+COL1 = 20   # label column width
+COL2 = 20   # value column width
+TOTAL = COL1 + COL2
+
+title = "Travel Expenses"
+print(f"\n{'-'*12}{title:^{TOTAL-24}}{'-'*12}")     # Title line with 12 dashes on each side
+print(f"{'Location:':<{COL1}}{destination:<{COL2}}")    # Location line with 2 columns, pull from user input
+print(f"{'Initial Budget:':<{COL1}}{money(budget):<{COL2}}")     # Initial budget line with 2 columns
+print(f"{'Fuel:':<{COL1}}{money(gas_money):<{COL2}}")   # Fuel line with 2 columns
+print(f"{'Accommodation:':<{COL1}}{money(hotel_money):<{COL2}}")    # Accommodations line with 2 columns
+print(f"{'Food:':<{COL1}}{money(food_money):<{COL2}}")  # Food line with 2 columns
+print("-" * TOTAL)  # brackets the travel expenses brief with dashes using the TOTAL value established above and used in the title line
+
+print(f"\nRemaining Balance: {money(remaining_balance)}") # Remaining balance line
